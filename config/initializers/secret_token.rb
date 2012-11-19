30d21baabaf6770c6943f3e1b6a9eca48ff11e63
Tony Arcieri <tony.arcieri@gmail.com>
2012-11-19 12:25:01 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueWeb::Application.config.secret_token = '76d78e4916e4b2332a22f9c754f05c00f6b3cf66029fbab4a0a82f602358bafdf1697293a032eceb401a7629fa323139f0720391fb7e234326855d0732f1d603'
