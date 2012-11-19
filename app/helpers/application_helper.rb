51427abaf1188fcfdd7eec12c5e646f254d46db0
Tony Arcieri <tony.arcieri@gmail.com>
2012-11-19 13:04:23 -0800

    Basic layout and home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def tabs
+    %w(overview working failed queues workers stats)
+  end
+
+  def tab(name)
+    raw "<li><a href=\"/#{name}\">#{name.capitalize}</a></li>"
+  end
 end
