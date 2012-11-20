70e676fb6b68a06d9394b43e14b71b753c6e7448
Tony Arcieri <tony.arcieri@gmail.com>
2012-11-20 11:31:37 -0800

    Key stats

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -6,4 +6,19 @@ module StatsHelper
   def redis_info
     Resque.redis.info.to_a.sort_by { |i| i[0].to_s }
   end
+
+  def key_size(key)
+    case Resque.redis.type(key)
+    when 'none'
+      0
+    when 'list'
+      Resque.redis.llen(key)
+    when 'set'
+      Resque.redis.scard(key)
+    when 'string'
+      Resque.redis.get(key).length
+    when 'zset'
+      Resque.redis.zcard(key)
+    end
+  end
 end
