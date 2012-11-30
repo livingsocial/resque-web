f040dece9637c5a3ce6e93ddaf8447efaa82a75d
Tony Arcieri <tony.arcieri@gmail.com>
2012-11-30 15:47:22 -0800

    Queues#show

--- app/helpers/queues_helper.rb
+++ app/helpers/queues_helper.rb
@@ -3,8 +3,28 @@ module QueuesHelper
     Resque.queues.sort_by(&:to_s)
   end
 
-  def queue_size(queue_name)
+  def queue_start_at
+    params[:start].to_i
+  end
+
+  def queue_end_at
+    if queue_start_at + queue_per_page > queue_size
+      queue_size
+    else
+      queue_start_at + queue_per_page
+    end
+  end
+
+  def queue_per_page
+    20
+  end
+
+  def queue_size(queue_name = params[:id])
     Resque.size queue_name
+  end
+
+  def queue_jobs
+    @queue_jobs ||= Resque.peek(params[:id], queue_start_at, queue_per_page)
   end
 
   def failed_queue_names
