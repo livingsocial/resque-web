1e94ae4d19adcdb857980a78498f64d653e8d6c2
Tony Arcieri <tony.arcieri@gmail.com>
2012-11-30 17:07:45 -0800

    Initial retry support

--- app/controllers/retry_controller.rb
+++ app/controllers/retry_controller.rb
@@ -1,2 +1,6 @@
 class RetryController < ApplicationController
+  def create
+    Resque::Failure.requeue_queue Resque::Failure.job_queue_name(params[:failure_id])
+    redirect_to failure_path(params[:failure_id])
+  end
 end
