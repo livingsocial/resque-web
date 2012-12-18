ad6b38f80760b7568e7997c1021b4fb1b4d4e368
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-17 17:13:55 -0800

    Initial RetryControllerTest

---
+++ test/functional/retry_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class RetryControllerTest < ActionController::TestCase
+  test "should retry jobs" do
+    failure_id = 1
+    post 'create', :failure_id => failure_id
+    assert_redirected_to failure_path(1)
+  end
+end
