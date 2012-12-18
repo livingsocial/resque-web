db2ab02a724725ccfdddd5e9e62fd2feabe085a3
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-17 18:23:17 -0800

    Initial FailuresControllerTest

--- test/functional/failures_controller_test.rb
+++ test/functional/failures_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class FailuresControllerTest < ActionController::TestCase
-  test "destroys failures" do
+  let(:exception) { RuntimeError.new("some exception") }
+  let(:worker)    { Resque::Worker.new(:test) }
+  let(:queue)     { "test_queue" }
+  let(:payload)   { Hash.new('class' => Object, 'args' => 3) }
+
+  it "destroys failures" do
+    Resque::Failure.create(
+      :exception => exception,
+      :worker    => worker,
+      :queue     => queue,
+      :payload   => payload
+    )
+    Resque::Failure.count.wont_equal 0
+
     post 'destroy'
+    Resque::Failure.count.must_equal 0
     assert_redirected_to failures_path
   end
 end
