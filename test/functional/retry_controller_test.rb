a91c6153547746400a3b61d5a8a6b577c40d0de1
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-17 18:47:30 -0800

    Create a real failure for the retry test

--- test/functional/retry_controller_test.rb
+++ test/functional/retry_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class RetryControllerTest < ActionController::TestCase
+  let(:exception) { RuntimeError.new("some exception") }
+  let(:worker)    { Resque::Worker.new(:test) }
+  let(:queue)     { "test_queue" }
+  let(:payload)   { Hash.new('class' => Object, 'args' => 3) }
+
   it "should retry jobs" do
-    failure_id = 1
+     failure_id = Resque::Failure.create(
+      :exception => exception,
+      :worker    => worker,
+      :queue     => queue,
+      :payload   => payload
+    )
+
     post 'create', :failure_id => failure_id
-    assert_redirected_to failure_path(1)
+    assert_redirected_to failure_path(failure_id)
   end
 end
