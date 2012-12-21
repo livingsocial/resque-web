c7473210d0aa1ed5954b738f7fb6c7892bde3324
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-20 17:04:03 -0800

    Support for deleting queues

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -2,4 +2,9 @@ class QueuesController < ApplicationController
   def show
     set_subtabs view_context.queue_names
   end
+
+  def destroy
+    Resque.remove_queue(params[:id])
+    redirect_to queues_path
+  end
 end
--- test/functional/queues_controller_test.rb
+++ test/functional/queues_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class QueuesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  let(:queue_name) { 'example_queue' }
+
+  it "deletes queues" do
+    Resque.push(queue_name, :class => 'ExampleJob')
+    Resque.queues.include?(queue_name).must_equal true
+
+    delete :destroy, :id => queue_name
+    assert_redirected_to queues_path
+
+    Resque.queues.include?(queue_name).wont_equal true
+  end
 end
