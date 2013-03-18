feed1ec267555cc7dbe909341251226fb69577ed
Tony Arcieri <tony.arcieri@gmail.com>
2013-03-18 13:36:40 -0700

    Avoid checking in the secret token (fixes #1)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,15 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ResqueWeb::Application.config.secret_token = '76d78e4916e4b2332a22f9c754f05c00f6b3cf66029fbab4a0a82f602358bafdf1697293a032eceb401a7629fa323139f0720391fb7e234326855d0732f1d603'
+
+if ENV["SECRET_TOKEN"]
+  ResqueWeb::Application.config.secret_token = ENV["SECRET_TOKEN"]
+else
+  if Rails.env.production?
+    raise "You must set ENV[\"SECRET_TOKEN\"] in your app's config vars"
+  elsif Rails.env.development? || Rails.env.test?
+    # Generate a random secret key
+    ENV["SECRET_TOKEN"] = ResqueWeb::Application.config.secret_token = SecureRandom.hex(30)
+  end
+end
+
