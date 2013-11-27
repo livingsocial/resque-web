e6fc303793f72539081ebbd7a57d95910efdd7e0
Marcio Trindade <marciotrindade@gmail.com>
2013-11-27 00:52:44 -0200

    testing WorkersController

--- test/functional/workers_controller_test.rb
+++ test/functional/workers_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
-class WorkersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+module ResqueWeb
+  class WorkersControllerTest < ActionController::TestCase
+    include ControllerTestHelpers
+
+    setup do
+      @routes = Engine.routes
+    end
+
+    describe "GET /index" do
+      it "renders the index page" do
+        visit(:index)
+        assert_template :index
+      end
+    end
+
+    describe "GET /show" do
+      it "renders the show page" do
+        visit(:show, id: "all")
+        assert_template :show
+      end
+    end
+
+  end
 end
