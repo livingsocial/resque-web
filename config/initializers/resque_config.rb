60ccc51e196b728cfe98dbf9a9524079a0f6905d
Kirill Platonov <mail@kirillplatonov.com>
2017-04-04 09:28:14 +0300

    Improve RAILS_RESQUE_REDIS condition

--- config/initializers/resque_config.rb
+++ config/initializers/resque_config.rb
@@ -3,6 +3,6 @@ require 'resque'
 # Previously, this initializer always set Resque.redis, and defaulted to '127.0.0.1:6379' if
 # RAILS_RESQUE_REDIS was not set.
 
-if ENV['RAILS_RESQUE_REDIS'] && ENV['RAILS_RESQUE_REDIS'].length
+if ENV['RAILS_RESQUE_REDIS'].present?
   Resque.redis = ENV['RAILS_RESQUE_REDIS']
 end
